42671e29126c2a846c1d5148896578e1fdb8a5d4
kiterai <kiterai@foolslab.net>
2022-08-22 20:42:13 +0900

    added cli tool

---
+++ masacarri/src/bin/cli.rs
@@ -0,0 +1,22 @@
+use dotenv::dotenv;
+
+use masacarri::comment::*;
+use masacarri::db::*;
+use masacarri::page::*;
+
+fn add_user() {
+    
+}
+
+fn delete_user() {
+    
+}
+
+fn update_password() {
+    
+}
+
+fn main() {
+    dotenv().ok();
+
+}
