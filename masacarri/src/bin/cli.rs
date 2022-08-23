4d17d924202342fbdbde31c55a716ba57ed9274f
kiterai <kiterai@foolslab.net>
2022-08-23 12:01:48 +0900

    defiened commands

--- masacarri/src/bin/cli.rs
+++ masacarri/src/bin/cli.rs
@@ -1,22 +1,55 @@
+use std::env;
+
 use dotenv::dotenv;
 
 use masacarri::comment::*;
 use masacarri::db::*;
 use masacarri::page::*;
 
-fn add_user() {
+fn add_user(username: &str) {
     
 }
 
-fn delete_user() {
+fn delete_user(username: &str) {
     
 }
 
-fn update_password() {
-    
+fn list_users() {
+
+}
+
+fn list_pages() {
+
+}
+
+fn update_password(username: &str) {
+
 }
 
 fn main() {
     dotenv().ok();
+    let args: Vec<String> = env::args().collect();
+    
+    match args[1].as_str() {
+        "adduser" => {
+            add_user(args[2].as_str());
+        },
+        "deluser" => {
+            delete_user(args[2].as_str());
+        },
+        "list" => {
+            match args[2].as_str() {
+                "user" => list_users(),
+                "pages" => list_pages(),
+                item => eprintln!("unknown list item: '{}'", item),
+            }
+        },
+        "passwd" => {
+            update_password(args[2].as_str());
+        },
+        cmd => {
+            eprintln!("unknown command: '{}'", cmd);
+        },
+    }
 
 }
