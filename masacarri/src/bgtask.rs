e72b314b83461d27e628b4945d9da9347d0afd56
kiterai <kiterai@foolslab.net>
2022-08-27 22:15:02 +0900

    removed task actor debug log

--- masacarri/src/bgtask.rs
+++ masacarri/src/bgtask.rs
@@ -8,17 +8,15 @@ pub struct BgActor;
 impl actix::Actor for BgActor {
     type Context = actix::SyncContext<Self>;
 
-    fn started(&mut self, _: &mut SyncContext<Self>) {
-        println!("Bgtask actor spawned");
-    }
+    fn started(&mut self, _: &mut SyncContext<Self>) {}
 }
 
 pub type BgTaskManager = Addr<BgActor>;
 
 pub fn make_bgtask_manager() -> Addr<BgActor> {
     let bgtask_threadnum = env::var("BGTASK_THREADNUM")
-    .unwrap_or("16".to_string())
-    .parse::<usize>()
-    .expect("BGTASK_THREADNUM is invalid");
+        .unwrap_or("16".to_string())
+        .parse::<usize>()
+        .expect("BGTASK_THREADNUM is invalid");
     SyncArbiter::start(bgtask_threadnum, move || BgActor::default())
 }
